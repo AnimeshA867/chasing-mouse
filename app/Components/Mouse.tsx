d4642ba97f725aa4fde993cdcefe97aa5d34a349
AnimeshA867 <animeshacharya867+github@gmail.com>
2023-11-13 22:29:26 +0545

    Changed style

--- app/Components/Mouse.tsx
+++ app/Components/Mouse.tsx
@@ -34,14 +34,14 @@ const Mouse = () => {
 
   return (
     <div className="h-screen w-screen overflow-hidden relative">
-      <motion.h1 className="text-[25px] md:text-[50px] font-bold w-full text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <motion.h1 className="text-[25px] md:text-[50px] font-bold md:w-4/5 w-full text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-purple-400 to-blue-500">
           This is Mr. Chassy and Mrs. Chassy. Wherever Mrs. Chassy goes, Mr.
           Chassy would follow. Run if you can.
         </span>
       </motion.h1>
       <motion.div
-        className="h-fit w-fit absolute"
+        className="h-fit w-fit absolute pointer-events-none"
         ref={ref}
         animate={{ x, y }}
         transition={{
@@ -52,10 +52,16 @@ const Mouse = () => {
           restDelta: 0.001,
         }}
       >
-        <Image src={"/mouse.png"} height={300} width={300} alt="Mr. Chassy" />
+        <Image
+          src={"/mouse.png"}
+          height={300}
+          width={300}
+          alt="Mr. Chassy"
+          className="pointer-event-none"
+        />
       </motion.div>
       <motion.div
-        className="h-fit w-fit absolute"
+        className="h-fit w-fit absolute pointer-events-none"
         drag
         ref={ref}
         animate={{ x, y }}
@@ -68,7 +74,7 @@ const Mouse = () => {
           height={300}
           width={300}
           alt="Mrs. Chassy"
-          className=""
+          className="pointer-events-none"
         />
       </motion.div>
     </div>
